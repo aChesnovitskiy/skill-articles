2a764e9ed2a86122abc97856d71812ff420c72e8
aChesnovitskiy <a.chesnovitskiy@gmail.com>
2020-03-19 07:48:33 +0300

    six Submenu behaviors

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/BottombarBehavior.kt
@@ -2,15 +2,16 @@ package ru.skillbranch.skillarticles.ui.custom.behaviors
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
 import kotlin.math.max
 import kotlin.math.min
 
-// TODO study https://android.jlelse.eu/scroll-your-bottom-navigation-view-away-with-10-lines-of-code-346f1ed40e9e
 class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
     CoordinatorLayout.Behavior<V>(context, attrs) {
+    // Instruct CoordinatorLayout that we care about vertical scroll events
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
@@ -22,6 +23,7 @@ class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
+    // Get the scroll event before the nested scrolling child (target) receives it
     override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
@@ -32,6 +34,10 @@ class BottombarBehavior<V : View>(context: Context, attrs: AttributeSet) :
         type: Int
     ) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+
+        // Clamp translationY (vertical location of this view relative to its top position)
         child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+
+        Log.d("My_BottombarBehavior", "translationY: ${child.translationY}")
     }
 }
--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -2,14 +2,19 @@ package ru.skillbranch.skillarticles.ui.custom.behaviors
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.ViewCompat
+import ru.skillbranch.skillarticles.R
 import kotlin.math.max
 import kotlin.math.min
 
 class SubmenuBehavior<V : View>(context: Context, attrs: AttributeSet) :
     CoordinatorLayout.Behavior<V>(context, attrs) {
+    private val appContext = context
+
+    // Instruct CoordinatorLayout that we care about vertical scroll events
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
@@ -21,6 +26,7 @@ class SubmenuBehavior<V : View>(context: Context, attrs: AttributeSet) :
         return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
 
+    // Get the scroll event before the nested scrolling child (target) receives it
     override fun onNestedPreScroll(
         coordinatorLayout: CoordinatorLayout,
         child: V,
@@ -31,6 +37,24 @@ class SubmenuBehavior<V : View>(context: Context, attrs: AttributeSet) :
         type: Int
     ) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
-        child.translationY = max(0f, min(child.height.toFloat(), child.translationY + dy))
+
+        // Calculate the ratio between height and width
+        val ratio = child.width / child.height
+
+        // Get margins of submenu
+        val margin = appContext.resources.getDimension(R.dimen.spacing_small_8)
+
+        // Set and clamp translationY (vertical location of this view relative to its top position)
+        child.translationY =
+            max(0f, min(child.height.toFloat() + margin, child.translationY + dy))
+
+        // Set and clamp translationX (horizontal location of this view relative to its left position)
+        child.translationX =
+            max(0f, min(child.width.toFloat() + margin, child.translationX + dy * ratio))
+
+        Log.d(
+            "My_SubmenuBehavior",
+            "translationY: ${child.translationY}, translationX: ${child.translationX}"
+        )
     }
 }
