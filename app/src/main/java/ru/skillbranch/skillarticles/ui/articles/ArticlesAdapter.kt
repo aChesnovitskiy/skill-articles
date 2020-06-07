633edb831ac44552a7e1fb9bae994d85dac64c12
aChesnovitskiy <a.chesnovitskiy@gmail.com>
2020-06-07 06:44:10 +0300

    [ADDED] ArticleItemView

--- app/src/main/java/ru/skillbranch/skillarticles/ui/articles/ArticlesAdapter.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/articles/ArticlesAdapter.kt
@@ -20,12 +20,14 @@ import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.data.ArticleItemData
 import ru.skillbranch.skillarticles.extensions.dpToIntPx
 import ru.skillbranch.skillarticles.extensions.format
+import ru.skillbranch.skillarticles.ui.custom.ArticleItemView
 
 class ArticlesAdapter(private val listener: (ArticleItemData) -> Unit) :
         ListAdapter<ArticleItemData, ArticleVH>(ArticleDiffCallback()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleVH {
-        val containerView = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
-        return ArticleVH(containerView)
+        val view = ArticleItemView(parent.context)
+//        val containerView = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
+        return ArticleVH(view)
     }
 
     override fun onBindViewHolder(holder: ArticleVH, position: Int) {
@@ -41,30 +43,31 @@ class ArticleDiffCallback : DiffUtil.ItemCallback<ArticleItemData>() {
 
 class ArticleVH(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
     fun bind(item: ArticleItemData, listener: (ArticleItemData) -> Unit) {
-        val posterSize = containerView.context.dpToIntPx(64)
-        val cornerRadius = containerView.context.dpToIntPx(8)
-        val categorySize = containerView.context.dpToIntPx(40)
+//        val posterSize = containerView.context.dpToIntPx(64)
+//        val cornerRadius = containerView.context.dpToIntPx(8)
+//        val categorySize = containerView.context.dpToIntPx(40)
+//
+//        Glide.with(containerView.context)
+//                .load(item.poster)
+//                .transform(CenterCrop(), RoundedCorners(cornerRadius))
+//                .override(posterSize)
+//                .into(iv_poster)
+//
+//        Glide.with(containerView.context)
+//                .load(item.categoryIcon)
+//                .transform(CenterCrop(), RoundedCorners(cornerRadius))
+//                .override(categorySize)
+//                .into(iv_category)
+//
+//        tv_date.text = item.date.format()
+//        tv_author.text = item.author
+//        tv_title.text = item.title
+//        tv_description.text = item.description
+//        tv_likes_count.text = "${item.likeCount}"
+//        tv_comments_count.text = "${item.commentCount}"
+//        tv_read_duration.text = "${item.readDuration} min read"
 
-        Glide.with(containerView.context)
-                .load(item.poster)
-                .transform(CenterCrop(), RoundedCorners(cornerRadius))
-                .override(posterSize)
-                .into(iv_poster)
-
-        Glide.with(containerView.context)
-                .load(item.categoryIcon)
-                .transform(CenterCrop(), RoundedCorners(cornerRadius))
-                .override(categorySize)
-                .into(iv_category)
-
-        tv_date.text = item.date.format()
-        tv_author.text = item.author
-        tv_title.text = item.title
-        tv_description.text = item.description
-        tv_likes_count.text = "${item.likeCount}"
-        tv_comments_count.text = "${item.commentCount}"
-        tv_read_duration.text = "${item.readDuration} min read"
-
+        (containerView as ArticleItemView).bind(item)
         itemView.setOnClickListener { listener(item) }
     }
 }
